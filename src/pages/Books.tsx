b55108024270832c134ed859bda277ba9704a1cf
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-02 08:05:17 +0600

    feat: implement search functionality in books page

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,4 +1,4 @@
-import { Col, Input, Row } from "antd";
+import { Col, Input, Row, theme } from "antd";
 import { Fragment } from "react/jsx-runtime";
 import FilterSidebar from "../features/books/components/FilterSidebar";
 import Sort from "../features/books/components/Sort";
@@ -6,11 +6,24 @@ import BookCard from "../components/common/BookCard";
 import { useMediaQuery } from "react-responsive";
 import { useGetAllProductsQuery } from "../features/books/api";
 import BookCardSkeleton from "../components/skeleton/BookCardSkeleton";
+import { useState } from "react";
+
+const { useToken } = theme;
 
 export default function Books() {
+  const { token } = useToken();
   const isSmallScreen = useMediaQuery({ maxWidth: 992 });
   const { data: booksData, isFetching } = useGetAllProductsQuery(undefined);
   const skeletonArray = Array.from({ length: 8 });
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredBooks =
+    booksData?.data?.filter((book) =>
+      ["title", "author", "category"].some((key) =>
+        book[key]?.toLowerCase()?.includes(searchQuery.toLowerCase())
+      )
+    ) || [];
 
   return (
     <Fragment>
@@ -39,7 +52,9 @@ export default function Books() {
           >
             <Input.Search
               size="large"
-              placeholder="Search books..."
+              placeholder="Search books by title, author, or category..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
               style={{ width: isSmallScreen ? "100%" : "60%" }}
             />
             {/* Hide Sort on small screens */}
@@ -48,14 +63,14 @@ export default function Books() {
 
           {/* Books Grid */}
           <Row gutter={[16, 16]}>
-            {/*  */}
             {isFetching
               ? skeletonArray.map((_, index) => (
                   <Col xs={24} sm={12} md={12} lg={8} xxl={6} key={index}>
                     <BookCardSkeleton />
                   </Col>
                 ))
-              : booksData?.data?.map((book, _id) => (
+              : filteredBooks?.length > 0
+              ? filteredBooks?.map((book, _id) => (
                   <Col xs={24} sm={12} md={12} lg={8} xxl={6} key={_id}>
                     <BookCard
                       id={book._id}
@@ -65,7 +80,17 @@ export default function Books() {
                       rating={book.rating}
                     />
                   </Col>
-                ))}
+                ))
+              : !isFetching && (
+                  <Col
+                    span={24}
+                    style={{ textAlign: "center", marginTop: "20px" }}
+                  >
+                    <h3 style={{ color: token.colorTextSecondary }}>
+                      No books found!
+                    </h3>
+                  </Col>
+                )}
           </Row>
         </Col>
       </Row>
